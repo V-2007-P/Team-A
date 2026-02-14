e9a9a6f554b73a3b1c9319e2496057454d98d162
vijayprasad <vijayprasad18ss@gmail.com>
2026-02-14 23:59:38 +0530

    Fresh start: Correct cloud config and image handling

--- db.js
+++ db.js
@@ -1,18 +1,4 @@
 
-// import { drizzle } from "drizzle-orm/mysql2";
-// import mysql from "mysql2/promise";
-// import * as schema from "./schema.js";
-
-// const connection = await mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// port: Number(process.env.DB_PORT) // 👈 Converts string to number
-// });
-
-// export const db = drizzle(connection, { schema, mode: "default" });
-
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "./schema.js";
